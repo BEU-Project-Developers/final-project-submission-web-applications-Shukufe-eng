48dccf2817f7f02559dd01f50a1f3bc94611a623
Shukufa Mammadli <shukufa@Shukufas-MacBook-Air.local>
2025-05-25 00:33:13 +0400

    First commit: Add c#FinalProject files

---
+++ HospitalManagementSystem/Controllers/StarterPageController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalManagementSystem.Controllers
+{
+    public class StarterPageController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
